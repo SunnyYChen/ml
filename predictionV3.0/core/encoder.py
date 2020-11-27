4ab043e56571fb70f67105e7bb73d09171f4e6c1
dongzhiwen <dongzhiwen@patsnap.com>
2020-11-27 12:46:03 +0800

    update encoder.py

--- predictionV3.0/core/encoder.py
+++ predictionV3.0/core/encoder.py
@@ -1,24 +1,8 @@
-def enctry(s, k):
-    encry_str = ""
-    for i, j in zip(s, k):
-        temp = str(ord(i) + ord(j)) + '_'
-        encry_str = encry_str + temp
-    return encry_str
-
-
-def dectry(p, k):
-    dec_str = ""
-    for i, j in zip(p.split("_")[:-1], k):
-        temp = chr(int(i) - ord(j))
-        dec_str = dec_str + temp
-    return dec_str
-
+import base64
 
 if __name__ == '__main__':
-    k = 'hanbaowang'
-    data = "i love u"
-    print("原始数据为：", data)
-    enc_str = enctry(data, k)
-    print("加密数据为：", enc_str)
-    dec_str = dectry(enc_str, k)
-    print("解密数据为：", dec_str)
+    msg = "你是傻乎乎的"
+    bytes_msg = msg.encode("utf-8")
+    print(base64.b64encode(bytes_msg))
+    msg = "5L2g5piv5YK75LmO5LmO55qE"
+    print(base64.b64decode(msg).decode("utf-8"))
